35fc39c4161ef1472df028c6bd111705f9310bd6
furetur <furetur@gmail.com>
2021-05-21 14:18:29 +0500

    commit

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install ffmpeg libsm6 libxext6  -y
+apt-get install -y python3-opencv
 wget https://pjreddie.com/media/files/yolov3.weights
 mv yolov3.weights ${BUILD_DIR}/modelconfig/yolov3.weights
